902c566741981fbe2635ad6c754264e983913070
Roman Zharov <rzharov.dev@gmail.com>
2024-09-29 12:19:52 +0300

    lessons 9,10 additinal info

--- 09 Access modifier/classes/BookProduct.php
+++ 09 Access modifier/classes/BookProduct.php
@@ -5,7 +5,6 @@ class BookProduct extends Product {
 	public function __construct($name, $price, $numPages) {
 
 		parent::__construct($name, $price);
-		//этой строкой мы обратились к родительскому классу и вызвали его метод в дочернем классе. Можно вызывать любой метод.
 
 		$this->numPages = $numPages;
 
@@ -19,9 +18,9 @@ class BookProduct extends Product {
 	}
 
 	public function getProduct() {
-		$out = parent::getProduct(); // получили в переменную результат работы родительского класса
+		$out = parent::getProduct();
  
-		$out .= "Кол-во страниц: {$this->numPages}<br>"; // дописали новую функциональность в дочернем классе 
+		$out .= "Кол-во страниц: {$this->numPages}<br>";
 		$out .= "Цена без скидки: {$this->price}<br>"; 
 		$out .= "Скидка: {$this->getDiscount()}%<br>"; 
 
